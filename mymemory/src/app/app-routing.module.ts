d00c6388af97968183711eb9762aae39a4f10cde
sgjh4040 <sgjh4040@naver.com>
2019-08-05 00:31:59 +0900

    -node.js 서버와 연동시키기 위한 authservice,authgaurd service 추가 -login 페이지 구성 -login, regist method 구성

--- mymemory/src/app/app-routing.module.ts
+++ mymemory/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'login', loadChildren: './member/login/login.module#LoginPageModule' },
+  { path: 'main', loadChildren: './pages/main/main.module#MainPageModule',canActivate: [AuthGuardService]},
 ];
 
 @NgModule({
