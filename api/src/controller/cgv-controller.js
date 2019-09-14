968251243429ec754ab15545974be0571a5978c5
sgjh4040 <sgjh4040@naver.com>
2019-09-14 17:09:56 +0900

    -Readme.md

--- api/src/controller/cgv-controller.js
+++ api/src/controller/cgv-controller.js
@@ -56,43 +56,28 @@ var detailmovie = (req,res)=>{
     client.fetch(detailurl,param,function(err,$,res1){
         let results = new Object();
         let stillcuts = [];
-    
         if(err){
             console.log("Error",err);
             return;
         }
-        //title
         results.title1 = $(".title").find('strong').text();
         results.title2 = $(".title").find('p').text();
 
-        //thumbnial 이미지
         results.thumbImage= $(".thumb-image").children().attr('src');
-
         let spec =$(".spec").text().replace(/(\s*)/g, "");
         let genre =spec.indexOf('장르');
-        //spec
+        
         results.spec1 = spec.substring(0,genre);
         results.spec2 = spec.substring(genre);
 
-        //예매율
-        // console.log("예매율",$(".percent").text());
         results.rate = $(".score").children(".percent").text();
-    
-        // console.log("실관람객수:",$(".view").text());
         results.view = $(".view").text();
-        
-        // console.log("spec:",$(".spec").text());
-    
-        // console.log("줄거리:",$(".sect-story-movie").text());
         results.overview =$(".sect-story-movie").text().replace(/\n/g,"").replace(/\/r/g,"").replace(/(^\s*)|(\s*$)/g, "")
-    
-        // console.log("sect-stillcut",$(".sect-stillcut").text());
         $(".sect-stillcut").find("img").each(function(idx){
             console.log($(this).attr('data-src'));
             stillcuts.push($(this).attr('data-src'));
         })
         results.stillcuts = stillcuts;
-        
         res.json(results);
     })
 }
